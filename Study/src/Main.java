9bd727733d54917f76f835a7039950052d7074b1
다용 <wou3768@gmail.com>
2022-03-19 20:55:33 +0900

    백준 1924

--- Study/src/Main.java
+++ Study/src/Main.java
@@ -4,9 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
-		System.out.println(a + b);
+		int month = scanner.nextInt();
+		int day = scanner.nextInt();
+
+		int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		String days[] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+		
+		for(int i = 0; i < month - 1; i++) {
+			day += daysInMonth[i];
+		}
+		System.out.println(days[day % 7]);
 		
 	}
 
