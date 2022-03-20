a4705ecc71b88859d1388838fa0de1bca4c170ad
다용 <wou3768@gmail.com>
2022-03-20 19:53:36 +0900

    백준 2439

--- Study/src/Main.java
+++ Study/src/Main.java
@@ -5,14 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int i = sc.nextInt();
+		sc.close();
 		for(int j = 0; j < i; j++) {
-			for(int k = 0; k < (j + 1); k++) {
+			for(int k = 0; k < (i - 1 - j); k++) {
+				System.out.print(" ");
+			}
+			for(int y = 0; y < (j + 1); y++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
-		
-		
 		
 		
 	}
