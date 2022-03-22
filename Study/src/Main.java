7c88a1e4e906f0ad8705c19418234e8a5aca0d49
다용 <wou3768@gmail.com>
2022-03-22 20:49:30 +0900

    백준 2739 구구단

--- Study/src/Main.java
+++ Study/src/Main.java
@@ -5,26 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		for(int i = 1; i < num + 1; i++) {
-			for(int k = 0; k < i - 1; k++) {
-				System.out.print(" ");
+		for(int i = num; i < num + 1; i++) {
+			for(int k = 1; k < 10; k++) {
+				System.out.println(num + " * " + k + " = " + (k*num));
 			}
-			for(int j = 0; j < 2 * num + 1 - (2 * i); j++) {
-				System.out.print("*");
-			}
-			System.out.println();
 		}
-		for(int y = 1; y < num ; y++) {
-			for(int r = 0; r < num - 1 - y; r++) {
-				System.out.print(" ");
-			}
-			for(int t = 0; t < (y * 2) + 1; t++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		
-		
 	}
 
 }
