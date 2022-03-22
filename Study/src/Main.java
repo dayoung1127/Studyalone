ac31b25c32422c62e30a3d61482f4b6a20ddd57d
다용 <wou3768@gmail.com>
2022-03-22 20:57:04 +0900

    백준 2742 기찍N

--- Study/src/Main.java
+++ Study/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		for(int i = 1; i < num + 1; i++) {
+		for(int i = num; i > 0; i--) {
 			System.out.println(i);
 		}
 	}
