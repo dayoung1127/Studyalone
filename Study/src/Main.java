310b52b25362c4c169c7acad78a82a96ef32b132
다용 <wou3768@gmail.com>
2022-03-22 20:19:57 +0900

    백준 2445 별찍기

--- Study/src/Main.java
+++ Study/src/Main.java
@@ -1,19 +1,37 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
 		for(int i = 0; i < num; i++) {
-			for(int j = 0; j < (num - 1 - i); j++) {
+			for(int j = 0; j < (i + 1); j++) {
+				System.out.print("*");
+			}
+			for(int k = 0; k < 2*(num - 1 - i); k++) {
 				System.out.print(" ");
 			}
-			for(int k = 0; k < (2 * i) + 1; k++) {
+			for(int t = 0; t < i + 1; t++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
+		for(int i = 0; i < num - 1; i++) {
+			for(int j = 0; j < num - 1 - i; j++) {
+				System.out.print("*");
+			}
+			for(int k = 0; k < 2*(i + 1); k++) {
+				System.out.print(" ");
+			}
+			for(int t = 0; t < num - 1 - i; t++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
 	}
-
 }
