dd119070b9c518e2f502c3b0dea310f3bbd12798
다용 <wou3768@gmail.com>
2022-03-22 21:01:24 +0900

    백준 8393 합

--- Study/src/Main.java
+++ Study/src/Main.java
@@ -5,9 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		for(int i = num; i > 0; i--) {
-			System.out.println(i);
+		int sum = 0;
+		for(int i = 1; i < num + 1; i++) {
+			sum += i;
 		}
+		System.out.println(sum);
 	}
 
 }
